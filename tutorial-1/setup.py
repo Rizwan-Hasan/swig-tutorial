90b1d87bd337ef92b71ab4f5f17655cd716b1453
Rizwan Hasan <rizwan.hasan486@gmail.com>
2018-12-01 23:34:29 +0600

    updated

--- tutorial-1/setup.py
+++ tutorial-1/setup.py
@@ -6,14 +6,13 @@ setup.py file for SWIG example
 
 from distutils.core import setup, Extension
 
-
 tutorial_module = Extension('_func',
                            sources=['func_wrap.cxx'],
                            )
 
 setup (name = 'func',
        version = '0.1',
-       author      = "Sanuj Sharma",
+       author      = "Rizwan Hasan",
        description = """Simple swig tutorial for c++ functions""",
        ext_modules = [tutorial_module],
        py_modules = ["func"],
